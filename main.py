23b5c926cb95b7ca73e8c62b72bd1294b0af5d7b
jove <joveluna@gmail.com>
2026-03-08 15:06:31 -0400

    Imported pyside6 for UI and setup basic application

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
-# This is a sample Python script.
+# This is a sample Python script with PySide6 UI.
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+from PySide6.QtWidgets import QApplication, QWidget
+import sys
 
 
-# Press the green button in the gutter to run the script.
+def main():
+    app = QApplication(sys.argv)
+    window = QWidget()
+    window.setWindowTitle("FingerPunch")
+    window.setGeometry(100, 100, 300, 200)
+    window.show()
+    sys.exit(app.exec())
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    main()

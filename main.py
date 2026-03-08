abd2941cf60248176f2b0bfab5628069d33fae8a
jove <joveluna@gmail.com>
2026-03-08 15:53:41 -0400

    Renamed typing_app.py to typingApp.py to align with camel case naming convention

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import sys
 from PySide6.QtWidgets import QApplication
-from typing_app import TypingPracticeApp
+from typingApp import TypingPracticeApp
 
 def main():
     app = QApplication(sys.argv)
